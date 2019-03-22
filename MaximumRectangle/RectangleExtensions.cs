c946861918cbd30dbace9d0a0bd038ea2821ca31
Dale Thompson <dale.thompson@vastec.com>
2019-03-22 12:39:00 -0400

    About as good as it is going to be before making it a component.

---
+++ MaximumRectangle/RectangleExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace MaximumRectangle
+{
+    using System.Drawing;
+
+    public static class RectangleExtensions
+    {
+        public static int Area(this Rectangle rectangle)
+        {
+            return rectangle.Width * rectangle.Height;
+        }
+    }
+}
--- MaximumRectangleTests/MaximumRectangleTests.cs
+++ MaximumRectangleTests/MaximumRectangleTests.cs
@@ -1,9 +1,9 @@
 ﻿namespace MaximumRectangleTests
 {
-    using System;
     using System.Collections.Generic;
     using System.Drawing;
     using System.Linq;
+    using MaximumRectangle;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
@@ -38,35 +38,36 @@
                 new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 11
             };
 
-            var result = MainBody(matrix);
+            var actual = MainBody(matrix);
 
-            Assert.AreEqual(new Rectangle(6, 4, 6, 2), result);
+            TestContext.WriteLine($"The maximal rectangle has area {actual.Area()}");
+            TestContext.WriteLine(
+                $"Location: [col={actual.X}, row={actual.Y}] to [col={actual.Right}, row={actual.Bottom}]");
+
+            var expected = new Rectangle(6, 4, 6, 2);
+            Assert.AreEqual(expected, actual);
         }
 
         private Rectangle MainBody(IList<int[]> matrix)
         {
-            var rows = matrix.Count();
-            var columns = matrix.First().Count();
+            var columns = matrix.First().Length;
 
             var lineCache = Enumerable.Repeat(0, columns + 1).ToList();
-            var widthStack = new Stack<Point>(Enumerable.Repeat(new Point(0,0), columns + 1));
+            var widthStack = new Stack<Point>(Enumerable.Repeat(new Point(0, 0), columns + 1));
 
-            var bestArea = 0;
-            var bestLowerLeft = new Point(0, 0);
-            var bestUpperRight = new Point(-1, -1);
+            var bestRectangle = Rectangle.Empty;
 
-            // main loop
-            for (var n = 0; n != rows; ++n)
+            for (var line = 0; line != matrix.Count; ++line)
             {
                 var openRectWidth = 0;
-                UpdateCache(lineCache, matrix[n]);
+                UpdateCache(lineCache, matrix[line]);
 
                 for (var m = 0; m != columns + 1; ++m)
                 {
                     if (lineCache[m] > openRectWidth)
                     {
                         /* Open new rectangle? */
-                        widthStack.Push(new Point( m, openRectWidth));
+                        widthStack.Push(new Point(m, openRectWidth));
                         openRectWidth = lineCache[m];
                     }
                     else if (lineCache[m] < openRectWidth)
@@ -80,11 +81,9 @@
                             w0 = val.Y;
 
                             var area = openRectWidth * (m - m0);
-                            if (area > bestArea)
+                            if (area > bestRectangle.Area())
                             {
-                                bestArea = area;
-                                bestLowerLeft = new Point(m0, n);
-                                bestUpperRight = new Point(m - 1, n - openRectWidth + 1);
+                                bestRectangle = new Rectangle(m0, line - openRectWidth + 1, m - m0, openRectWidth);
                             }
 
                             openRectWidth = w0;
@@ -100,21 +99,21 @@
                 }
             }
 
-            TestContext.WriteLine($"The maximal rectangle has area {bestArea}");
-            TestContext.WriteLine(
-                $"Location: [col={bestLowerLeft.X}, row={bestLowerLeft.Y}] to [col={bestUpperRight.X}, row={bestUpperRight.Y}]");
-
-            return new Rectangle(bestLowerLeft.X, bestUpperRight.Y,
-                Math.Abs(bestUpperRight.X - bestLowerLeft.X) + 1, Math.Abs(bestUpperRight.Y - bestLowerLeft.Y) + 1);
+            return bestRectangle;
         }
 
-        private void UpdateCache(IList<int> cache, IList<int> row)
+        private void UpdateCache(IList<int> cache, IList<int> newLine)
         {
-            for (var x = 0; x != row.Count; ++x)
+            for (var x = 0; x != newLine.Count; ++x)
             {
                 // TODO: will need to invert for images
-                cache[x] = row[x] == 0 ? 0 : cache[x] + 1;
+                cache[x] = IsSet(newLine[x]) ? 0 : cache[x] + 1;
             }
+        }
+
+        public bool IsSet(int value)
+        {
+            return value == 0;
         }
     }
 }
